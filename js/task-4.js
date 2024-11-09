a61923d8fbcbf62649c816405ea62da7c9ad6f81
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2024-11-09 22:55:13 +0100

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -22,6 +22,26 @@
 //* Якщо інпут містить текст, додай новий елемент <li> із цим текстом до списку <ul id="tasks">.
 //* Очисть значення інпуту після додавання завдання.
 
+// const form = document.querySelector("#task-form");
+// const list = document.querySelector("#tasks");
+
+// form.addEventListener("submit", handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const text = form.elements.text.value.trim(); // Отримуємо значення поля вводу і обрізаємо зайві пробіли
+//   if (text === "") {
+//     alert("Please enter a task");
+//     return;
+//   }
+
+//   const item = document.createElement("li");
+//   item.textContent = text;
+//   list.append(item); // Додаємо елемент до списку
+//   // Очищаємо поле вводу після додавання
+//   form.reset();
+// }
+
 //* Task2 Напиши скрипт, який створить список фільмів. Кожен елемент списку <li> повинен містити:
 //* Назву фільму у заголовку <h3>.
 //* Зображення постера <img> з відповідним src та alt.
@@ -42,11 +62,60 @@
 //   },
 // ];
 
+// const moviesList = document.querySelector(".movie-list");
+// moviesList.insertAdjacentHTML("beforeend", createMarkup(movies));
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ title, poster }) => `
+//       <li class="movie-item">
+//       <h3 class="movie-title">${title}
+//       </h3>
+//       <img src=${poster} alt=${title}/></li>
+// `
+//     )
+//     .join("");
+// }
+
 //* Task3 Є форма з полями для введення імені, електронної пошти та коментаря:
 //* При натисканні на кнопку "Submit Comment":
 //* Якщо всі поля заповнені, створити новий <li> у <ul class="comments-list">, який містить ім'я, email і коментар.
 //* Якщо хоча б одне поле пусте, вивести повідомлення про необхідність заповнення всіх полів.
 //* Після додавання коментаря очистити форму
+
+// const commentForm = document.querySelector(".comment-form");
+// const commentList = document.querySelector(".comments-list");
+
+// commentForm.addEventListener("submit", createList);
+
+// function createList(event) {
+//   event.preventDefault();
+
+//   const { name, email, comment } = commentForm.elements;
+//   const fields = [name.value.trim(), email.value.trim(), comment.value.trim()];
+
+//   if (fields.includes("")) {
+//     alert("Please fill in all fields.");
+//     return;
+//   }
+
+//   commentList.insertAdjacentHTML("beforeend", createMarkup(...fields));
+
+//   // Очищаємо форму після додавання коментаря
+//   commentForm.reset();
+// }
+
+// // Функція для створення HTML-розмітки на основі даних
+// function createMarkup(name, email, comment) {
+//   return `
+//     <li class="comment-item">
+//       <p><strong>Name:</strong> ${name}</p>
+//       <p><strong>Email:</strong> ${email}</p>
+//       <p><strong>Comment:</strong> ${comment}</p>
+//     </li>
+//   `;
+// }
 
 //* Task4 Напиши скрипт, який:
 //* Збільшує значення лічильника на 1 при натисканні на кнопку +1.
