6efcd20c23e0847279834198e10efc7591d602f1
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2024-11-11 20:26:54 +0100

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,12 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+const settings = {
+  theme: "dark",
+  isAuthenticated: true,
+  options: [1, 2, 3],
+};
+localStorage.setItem("settings", JSON.stringify(settings));
+
+const savedSettings = localStorage.getItem("settings");
+console.log(savedSettings); // A string // {"theme":"dark","isAuthenticated":true,"options":[1,2,3]}
+
+const parsedSettings = JSON.parse(savedSettings);
+console.log(parsedSettings); // Settings object // У змінній savedSettings буде рядок, що представляє об'єкт, тому ми розпарсюємо це значення, і у змінній parsedSettings отримуємо повноцінний об'єкт із властивостями.
